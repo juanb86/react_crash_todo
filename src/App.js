18b1e14a2411b3487ef4a802b8953edd3be5720a
juanb86 <juan.e.begnis@gmail.com>
2020-05-31 05:44:25 -0300

    Initialize React files structure

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./App.css";
+import Todos from "./components/Todos";
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "take out the trash",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "study React",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "have meal",
+        completed: false,
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
+}
+
+export default App;
